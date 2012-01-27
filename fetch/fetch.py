fa5e70518ad55b354cefe7447b157f64e89f62e7
Evan McClanahan <evan@evanmcc.com>
2012-01-27 13:06:05 -0800

    stopping point where feed requests are working but non-functional if that makes any sense

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 
 from gevent import monkey; monkey.patch_socket()
 from gevent import Greenlet as greenlet, sleep
@@ -6,17 +8,20 @@ from os import urandom, getpid
 from socket import gethostname
 from sys import exit
 
-from logger import info, debug, warn, error, INFO, DEBUG, WARN, ERROR, \
-    getLogger as get_logger, setLevel as set_level
+from logging import info, debug, warn, error, INFO, DEBUG, WARN, ERROR, \
+    getLogger as get_logger
 
-from feedsync import parse
+from feedparser import parse
 from itsdangerous import URLSafeTimedSerializer as url_serializer
 
+from random import randint
 from datetime import datetime
 from json import dumps, loads, load
-from urllib import urlencode 
-from urllib2 import Request as request, urlopen, HTTPError, URLError
-from httplib import HTTPConnection as http_connection
+from hashlib import sha256
+
+from requests import get, post, ConnectionError, request, Response
+#not sure how to working gevent support here
+#not sure it's needed, but maybe later
 
 '''
 fetch is the part of the site that does all of the feedparsing stuff.
@@ -55,7 +60,6 @@ class feed_watcher(greenlet):
 
         self.posts_queue = []
 
-
     def update(self):
         # determine if we should check for new posts
 
@@ -69,6 +73,9 @@ class feed_watcher(greenlet):
                 self.modified = update.modified
                 
                 #push an update to the feed state
+                
+                #will need some non-compliance detection here.
+
 
         for post in self.posts_queue:
             sleep(1)
@@ -88,6 +95,7 @@ class feed_watcher(greenlet):
 class feed_getter(greenlet):
     def __init__(self, server_addr, name, serializer):
         greenlet.__init__(self)
+        debug('init feed_getter: ' + server_addr + ' ' + name)#, serializer)
         self.server_addr = server_addr 
         self.name = name
         self.serializer = serializer
@@ -97,68 +105,105 @@ class feed_getter(greenlet):
     def register(self):
         # if the server goes away or loses our registration,
         # we should try again
-        ret =  rest_call(self.server_addr, 'register/', 'POST',
-                         self.serializer,
-                         { 'name':self.name, 
-                           'pid':getpid(),
-                           'host':gethostname() })
+        ret = rest_call(self.server_addr, 'register/', 'POST',
+                        self.serializer,
+                        values = { 'name':self.name, 
+                                   'pid':getpid(),
+                                   'addr':self.server_addr })
         
-        if ret != 200, 'OK':
+        if ret.status_code == 200:
+            return True
+        else: 
+            return False
             #check for name conflict and reregister if need be
-            exit(2)
+            
+        
 
     def _run(self):
-        self.register()
+        debug('starting up the main feed getter')
+        while not self.register():
+            sleep(5)
+
         while True:
-            feeds = self.get_feeds()
-            for feed in feeds: 
-                feed = feed_watcher(feed, self.server_addr) 
-                feed.spawn()
-                feed.start()
-            if not feeds:
-                sleep(30)
+            # see if the server has a feed for us
+            ret = rest_call(self.server_addr, 'feed/', 'GET',
+                            self.serializer, params = {'name':self.name})
+            try: 
+                if ret and ret.content:
+                    feed_url = loads(ret.content)
+                    feed = feed_watcher(feed_url, self.server_addr) 
+                    feed.spawn()
+                    feed.start()
+                else:
+                    ret = None
+            except Exception, e:
+                print e 
+                
+            if not ret: 
+                sleep(10)
 
 
+def rest_call(host, path, method, serializer, params = {}, values = {}):
 
-def rest_call(host, path, method, serializer, values = None):
-    if values:
-        values = urlencode(values)
-    con = http_connection(server_addr)
+    values = dict([ (x, unicode(y)) for x, y in values.items() ])
 
-    cryptkey = serializer.dumps(urandom(8))
-    con.request(method, '/fetch/' + path + '?key=' + cryptkey, values)
+    cksum = sha256(repr(values)).hexdigest()
+    cryptkey = serializer.dumps(cksum)
+    params.update({ 'key' : cryptkey })
 
-    response = con.getresponse()
+    debug('values: ' + repr(values))
 
-    if response.status == 200:
-        data = loads(response.data)
+    try: 
+        if method == "POST":
+            rep = post(host + '/fetch/' + path, params=params, data=values)
+        elif method == "GET":
+            rep = get(host + '/fetch/' + path, params=params)
+    except ConnectionError:
+        #not sure this is really the right thing to do
+        dummy = Response()
+        dummy.status_code = 500
+        return dummy
+        
+
+    if rep.status_code == 200:
+        print rep.content
+        if rep.content:
+            data = loads(rep.content)
+            print data
     else: 
-        raise Exception('blar')
-    return response
+        error('got status code %s' % rep.status_code)
+
+    return rep
 
 # global for ease of use.
 serializer = None
 
 def main():
-    global serializer 
+    global serializer, name
 
     name = 'fetcher' + str(randint(100, 9999))
 
-    cfg = load(open('fetch.cfg'))
+    cfg = load(open('/etc/fetch/fetch.cfg'))
 
     server_addr = cfg['server_addr']
 
     serializer = url_serializer(cfg['key'])
 
     get_logger().name = name
-    get_logger().set_level(cfg['log_level'])
+    get_logger().setLevel(cfg['log_level'])
+
+    info('starting up')
 
     fg = feed_getter(server_addr, name, serializer)
-    fg.spawn()
+    fg.start()
     fg.join()
 
+    warn('not sure why we\'re here')
     
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(0)
 
--- triage/feedsync/fetch.py
+++ triage/feedsync/fetch.py
@@ -7,63 +7,79 @@ from flask.views import MethodView
 from itsdangerous import URLSafeTimedSerializer as url_serializer, \
     BadSignature
 
-# feed utility functions
+from hashlib import sha256
+from pymongo import Connection
+
+# not sure this is thread-safe
 serializer = url_serializer("234jn13lk1jb4tgk4jg4kljgbq;lfasdFADFASDGj3rb3")
-fetchers = {}
 
+fetchers = Connection().sync_db.fetchers
 
-def signed_request(signstring): 
-    print signstring
+def signed_request(args, form): 
+    #form = dict(form)
+    signstring = args.get('key')
+
+    #werkzeug does some weird value munging here, have to undo
+    form = dict([ (x, y) for x, y in form.items() ])
+    payload = sha256(repr(form)).hexdigest()
+
     try: 
         ret = serializer.loads(signstring, max_age=1.5)
-        return ret
+        if payload == ret:
+            return True
     except BadSignature, e: 
         app.logger.warning('someone tried to pass us a bogus request: %s' % e)
         # need more info there
         return False
     return False
 
-def register(name, pid, hostname): 
+def register(name, pid, addr): 
+    
+
     # make sure we're not already registered.
-    if fetchers.get(name): 
+    if fetchers.find_one({'name':name}): 
+        app.log.warning('fetcher  %s already registered' % name)
         abort(403) #maybe different code?
+    
+    # we should also require that this is coming from the local subnet
 
-    print hostname, request.remote_addr#, request.remote_host
-
-    if request.remote_host != hostname: 
+    addr = addr.strip('http:/')
+    if addr.find(':') != -1:
+        addr = addr.split(':')[0]
+    print request.remote_addr, addr     
+    if request.remote_addr != addr: 
         abort(403) 
-    fetchers['name'] = {'pid':pid, 'hostname':hostname}
+    fetchers.insert({'name':name, 'pid':pid, 'addr':addr})
     return True
 
 def registered():
-    name = request.form.get('name')
-    pid = request.form.get('pid')
-    host = request.form.get('host')
-    if not name or not pid or not host:
+    name = request.args.get('name')
+
+    reg = fetchers.find_one({'name':name})
+    if not name or not reg:
+        app.logger.debug('with %s, got %s' % (name, reg))
         return False
-    reg = fetchers.get(name)
-    if not reg:
-        return False
-    if request.remote_host != host: 
+
+    if request.remote_addr != reg['addr']: 
         return False
     return True
 
 class fetch_register(MethodView):
     def get(self):
-        if signed_request(request.args.get('key')) and \
+        if signed_request(request.args, request.form) and \
                 registered():
             return ''
         else: 
             abort(401)
 
     def post(self):
-        if signed_request(request.args.get('key')):
+        if signed_request(request.args, request.form):
             name = request.form.get('name')
             pid = request.form.get('pid')
-            host = request.form.get('host')
-            print name, pid, host
-            if name and pid and host:
-                if register(name, pid, host):
+            addr = request.form.get('addr')
+
+            if name and pid and addr:
+                if register(name, pid, addr):
                     return ''
 
         abort(403)
@@ -72,9 +88,22 @@ app.add_url_rule('/fetch/register/', view_func=fetch_register.as_view('fetch_reg
 
 class fetch_feed(MethodView):
     def get(self):
-        if not signed_request(request.args.get('key')) or not registered():
+        if not signed_request(request.args, request.form) or not registered():
             abort(403)
         # determine how well the load is balanced.
+
+        # are there any unassigned feeds?
+        unassigned = [ x for x in feeds.find({'cat':'unassigned'}) ]
+        app.logger.debug('%s' % unassigned)
+        if not unassigned: 
+            return ''
+        abort(401)
+        # how many fetchers are alive?
+
+        # how many feeds are there?
+
+        # how many feeds are assigned to this fetcher?
+
 
         # if this client doesn't need any more stuff, return nothing
             
